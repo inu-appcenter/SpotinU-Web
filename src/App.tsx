ce9cc2d29f625118cc15f1b9237d07f1188de7d7
hxxneei <zxcvb5120@naver.com>
2025-08-28 07:38:01 +0900

    파일수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import BottomNavBar from './components/Common/BottomNavBar'
 import LoginPage from './pages/LoginPage'
 import MainPage from './pages/MainPage'
-// import MyCampusMap from './pages/MyCampusMap'
+import MyCampusMap from './pages/MyCampusMap'
 import PlaceDetails from './pages/PlaceDetails'
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/places" element={<PlaceDetails />} />
-        {/* <Route path="/my-campus-map" element={<MyCampusMap />} /> */}
+        <Route path="/my-campus-map" element={<MyCampusMap />} />
         <Route path="/my-page" element={<LoginPage />} />
       </Routes>
       <BottomNavBar />
