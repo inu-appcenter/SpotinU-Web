42230f5be3bd63ed75d2184d2c05ae4729657ab3
milin3t <minjunseong@naver.com>
2025-09-03 14:27:41 +0900

    Feat : Recent Visit 페이지 퍼블리싱만

---
+++ src/pages/RecentVisit.tsx
@@ -0,0 +1,11 @@
+import BackHeader from '@/components/Common/BackHeader'
+
+const RecentVisit = () => {
+  return (
+    <>
+      <BackHeader title="최근 방문 내역" />
+    </>
+  )
+}
+
+export default RecentVisit
